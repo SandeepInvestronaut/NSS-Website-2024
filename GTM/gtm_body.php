4b44087058667c0d26cbf48b3b1cec3a5090a189
SandeepInvestronaut <152471606+SandeepInvestronaut@users.noreply.github.com>
2024-09-25 15:33:28 +0530

    bract website update with our team page

---
+++ GTM/gtm_body.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GTM Body Tag</title>
+</head>
+<body>
+     <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5G9V2THQ" height="0" width="0"
+      style="display: none; visibility: hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+</body>
+</html>
--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Bract Website Development - 2024(Inv)
 HTML
 CSS and 
 JS
+
+
+Global Assets for whole webbsite.
+All child pages inside pages folder.
+index.php is main/home page.
+main.css is global css.
+main.js is global js.
+
+
